955a1945ef78e0826385dbac879ef6870c7d37e5
Osvaldofrancisco-oc <61027333+Osvaldofrancisco-oc@users.noreply.github.com>
2025-12-06 21:40:14 +0000

    day 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,37 @@
-const Hello = ({name, age}) => {
-  
-  console.log({name, age})
-  const bornYear = () =>{
-    const yearNow = new Date().getFullYear()
-    return yearNow - age
-  }
-  return (
-    <div>
-      <p>Hello {name}, you are {age} years old!</p>
-      <p>So you  were probably born in {bornYear()}</p>
-    </div>
-  )
+import { useState } from "react"
 
-}
+
+const Display = ({counter}) => <div>{counter}</div>
+
+
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
 const App = () =>{
+  const [counter, setCounter] = useState(0)
 
-  const name = 'Osvaldo'
-  const age = '24'
+  // const handleClick = () =>{
+  //   console.log('clicked')
+  // }
+  
+  // setTimeout(
+  //   () => setCounter(counter + 1),
+  //   1000
+  // )
 
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+  console.log('rendering...', counter)
+  
   return(
     <>
-    <p>Greetings</p>
-    <Hello name = "Canhama" age = {12 + 11}/>
-    <Hello name = {name} age = {age}/>
+      <Display  counter={counter}/>
+      <Button onClick = {increaseByOne} text = 'plus'/>
+      <Button onClick={decreaseByOne} text = 'minus'/>
+      <Button onClick={setToZero} text = 'Zero'/>
     </>
   )
 }
+
 export default App
